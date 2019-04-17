cac1bfc8f448894f4e8ecb3be88a5e1d81607544
Avinash Poddar <avinashpoddar10@gmail.com>
2019-04-17 23:12:28 +0530

    Added a base class for test SetUp and TearDown

---
+++ Code/BaseSetup.cs
@@ -0,0 +1,35 @@
+﻿using Code.Helpers;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public class BaseSetup
+    {
+        [SetUp]
+        public virtual void TestSetUp()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--disable-notifications");
+            options.AddArguments("--start-maximized");
+            BrowserContext.CurrentDriver = new ChromeDriver(options);
+            BrowserContext.CurrentDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+
+        [TearDown]
+        public void TestTearDown()
+        {
+            BrowserContext.CurrentDriver.Quit();
+        }
+
+        public void NavigateTo(string url)
+        {
+            BrowserContext.CurrentDriver.Navigate().GoToUrl(url);
+        }
+    }
+}
