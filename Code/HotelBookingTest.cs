c6adff64ec67dfbf60894fc0638927e2719b82cc
Avinash Poddar <avinashpoddar10@gmail.com>
2019-04-17 07:59:03 +0530

    Created a new Unit Test Project and copied, all the 3 java test class in C#

---
+++ Code/HotelBookingTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public class HotelBookingTest
+    {
+
+        WebDriver driver = new ChromeDriver();
+
+        @FindBy(linkText = "Hotels")
+    private WebElement hotelLink;
+
+        @FindBy(id = "Tags")
+    private WebElement localityTextBox;
+
+        @FindBy(id = "SearchHotelsButton")
+    private WebElement searchButton;
+
+        @FindBy(id = "travellersOnhome")
+    private WebElement travellerSelection;
+
+        @Test
+    public void shouldBeAbleToSearchForHotels()
+        {
+            setDriverPath();
+
+            driver.get("https://www.cleartrip.com/");
+            hotelLink.click();
+
+            localityTextBox.sendKeys("Indiranagar, Bangalore");
+
+            new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
+            searchButton.click();
+
+            driver.quit();
+
+        }
+
+        private void setDriverPath()
+        {
+             if (PlatformUtil.isMac()) {
+                 System.setProperty("webdriver.chrome.driver", "chromedriver");
+             }
+             if (PlatformUtil.isWindows()) {
+                 System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+             }
+             if (PlatformUtil.isLinux()) {
+                 System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+             }
+        }
+
+    }
+}
---
+++ Code/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Code")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Code")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("69fe3c68-e577-45cc-a181-75a1a68b9c4a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
