b7827a7132eb8ed1eac754431098bc5c9ac0d540
Avinash Poddar <avinashpoddar10@gmail.com>
2019-04-17 21:42:46 +0530

    Added Hotels view, hotels tdo and updated HotelBookingTest as per the page object model

--- Code/HotelBookingTest.cs
+++ Code/HotelBookingTest.cs
@@ -1,43 +1,34 @@
 ﻿using Code.Helpers;
+using Code.Pages;
+using Code.TestDataObjects;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Support.PageObjects;
 
 namespace Code
 {
     public class HotelBookingTest
     {
-        IWebDriver driver;
-
-        BrowserContext Browser = new BrowserContext();        
-
-        [FindsBy(How = How.LinkText, Using = "Hotels")]
-        private IWebElement hotelLink;
-
-        [FindsBy(How = How.Id, Using = "Tags")]
-        private IWebElement localityTextBox;
-
-        [FindsBy(How = How.Id, Using = "SearchHotelsButton")]
-        private IWebElement searchButton;
-
-        [FindsBy(How = How.Id, Using = "travellersOnhome")]
-        private IWebElement travellerSelection;
-
-        public HotelBookingTest()
-        {
-            driver = Browser.LaunchChromeBrowser();
-            PageFactory.InitElements(driver, this);
-        }
-
         [Test]
         public void ShouldBeAbleToSearchForHotels()
         {
-            //Browser.SetDriverPath();
-            Browser.NavigateToUrl("https://www.cleartrip.com/");            
-            Browser.ClickElement(hotelLink);
-            Browser.SendText(localityTextBox, "Indiranagar, Bangalore");
-            Browser.SelectElementFromDropDown(travellerSelection, SelectBy.Text, "1 room, 2 adults");
-            Browser.ClickElement(searchButton);
+            //Arrange
+            HotelsTdo testdata = new HotelsTdo
+            {
+                Localtity = "Indiranagar, Bangalore",
+                TravellerSelection = "1 room, 2 adults"
+            };
+
+            BrowserContext Browser = new BrowserContext();
+            Browser.LaunchChromeBrowser();
+            HotelsView hotelsView = new HotelsView(Browser.CurrentDriver);
+            
+            //Act
+            Browser.NavigateToUrl("https://www.cleartrip.com/");
+            hotelsView.SearchHotels(Browser, testdata);
+
+            //Assert
+            // No assertion as per the test written
+
+            //Tear Down
             Browser.QuitDriver();
         }
     }
---
+++ Code/Pages/HotelsView.cs
@@ -0,0 +1,41 @@
+﻿using Code.Helpers;
+using Code.TestDataObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Pages
+{
+    class HotelsView
+    {
+        [FindsBy(How = How.LinkText, Using = "Hotels")]
+        private IWebElement hotelLink;
+
+        [FindsBy(How = How.Id, Using = "Tags")]
+        private IWebElement localityTextBox;
+
+        [FindsBy(How = How.Id, Using = "SearchHotelsButton")]
+        private IWebElement searchButton;
+
+        [FindsBy(How = How.Id, Using = "travellersOnhome")]
+        private IWebElement travellerSelection;
+
+        public HotelsView(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void SearchHotels(BrowserContext Browser, HotelsTdo testdata)
+        {
+            Browser.ClickElement(hotelLink);
+            Browser.SendText(localityTextBox, testdata.Localtity);
+            Browser.SelectElementFromDropDown(travellerSelection, SelectBy.Text, testdata.TravellerSelection);
+            Browser.ClickElement(searchButton);
+        }
+
+    }
+}
